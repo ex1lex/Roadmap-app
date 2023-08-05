96e2204a638dcccb01c3224d4e41860333cd4c64
Alexey Obryadin <obryadin.alexey@gmail.com>
2023-08-05 21:04:50 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ToastContainer } from 'react-toastify';
+import { BrowserRouter } from 'react-router-dom';
+
+import './styles/base/index.scss';
+import Routing from './core/routes';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	document.getElementById('root') as HTMLElement,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+	<React.StrictMode>
+		<ToastContainer />
+		<BrowserRouter>
+			<Routing />
+		</BrowserRouter>
+	</React.StrictMode>,
+);
