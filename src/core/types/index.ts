ab7321b806f68398698523a40c7302496b9aacb2
Alexey Obryadin <obryadin.alexey@gmail.com>
2023-08-09 12:21:30 +0800

    modify types

--- src/core/types/index.ts
+++ src/core/types/index.ts
@@ -10,12 +10,14 @@ export interface INewItem {
 	name: string;
 	tags: ITag[];
 	steps: INewStep[];
-	about: string;
+	about?: string;
+	createdAt: string;
 }
 
 export interface IStep extends INewStep {
 	id: string;
 	isSuccess: boolean;
+	steps?: IStep[];
 }
 
 export interface IItem extends INewItem {
